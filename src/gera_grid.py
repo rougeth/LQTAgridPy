f0052f7bd8960d7a47e305797fa625a53c526e3b
Mario <sergio.mario_q@hotmail.com>
2014-05-14 17:22:56 -0300

    Added class 'geraGrid'.

---
+++ src/gera_grid.py
@@ -0,0 +1,36 @@
+from intertools import izip
+
+class geraGrid():
+    """O construtor dessa classe lê um arquivo com a lista dos arquivos gro e top 
+       correspondentes aos resultados das dinâmicas para cada molécula
+       e gera o Grid para o conjunto de moléculas"""
+
+    def __init__(self, x, y, z, dx, dy, dz, atp, nameFile):
+        self.output = ""
+
+        n = len(atp)
+        for i in xrange(0,x):
+            for j in xrange(0,y):
+                for k in xrange(0,z):
+                    for l in xrange(0,n):
+                        self.output = self.output + "%d_%d_%d_%s_C \t" % (i+dx, j+dy, k+dz, atp[l])
+                        self.output = self.output + "%d_%d_%d_%s_LJ \t" % (i+dx, j+dy, k+dz, atp[l])
+
+        self.output = self.output + "\n"
+
+            
+            with open(nameFile) as f:
+                self.dataFile = f.readlines()
+
+            for lineGro, lineItp in pairwise(self.dataFile):
+                fileNameGro = lineGro
+                fileNameItp = lineItp
+                # GMC = GeraMatrizCoordenadas(fileNameGro, fileNameItp)
+                # GMC.geraGrid(x,y,z,atp,dx,dy,dz)
+                # output.format(GMC.salvarGrids());
+                # output.format("\n");
+    
+    def pairwise(iterable):
+        a = iter(iterable)
+        return izip(a, a)
+        
