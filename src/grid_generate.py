c8c87f5acf50750a2e8186a4cd6d2d83c472f480
Mario <sergio.mario_q@hotmail.com>
2014-08-16 15:49:39 -0300

    Adjust in step.

--- src/grid_generate.py
+++ src/grid_generate.py
@@ -5,14 +5,20 @@ import matrix_generate
 
 class GridGenerate():
 
-    def __init__(self, x0, y0, z0, dim_x, dim_dy, dim_dz, atp, fileGro, fileItp, step):
+    def __init__(self, x0, y0, z0, dim_x, dim_y, dim_z, atp, fileGro, fileItp, step):
         coulomb = ""
         lj = ""
         GMC = matrix_generate.MatrixGenerate(fileGro, fileItp)
 
-        I = int((dim_x/step)+(1/step-1))
-        J = int((dim_dy/step)+(1/step-1))
-        K = int((dim_dz/step)+(1/step-1))
+        if not step == 1: 
+            I = int((dim_x/step)+(1/step-1))
+            J = int((dim_y/step)+(1/step-1))
+            K = int((dim_z/step)+(1/step-1))
+        else:
+            I = dim_x + 1
+            J = dim_y + 1
+            K = dim_z + 1
+
         n = len(atp)
 
         GMC.gridGenerate(I, J, K, atp, x0, y0, z0, step)
